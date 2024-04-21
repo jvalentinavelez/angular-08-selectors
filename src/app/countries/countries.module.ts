728288954c95fc823378c19ab749343d3679cb61
Juanita Velez <jvalentina.velez@gmail.com>
2024-04-21 18:17:32 -0500

    Selector con regiones

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { CountriesRoutingModule } from './countries-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SelectorPageComponent],
-  imports: [CommonModule, CountriesRoutingModule],
+  imports: [CommonModule, CountriesRoutingModule, ReactiveFormsModule],
 })
 export class CountriesModule {}
--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
 
 @Component({
   templateUrl: './selector-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SelectorPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required],
+  });
 
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) {}
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
 }
